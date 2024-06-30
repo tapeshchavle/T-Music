0a887dbbedc1fbe4aea21691e059a3fb8d56f5c8
tapeshchavle <tapeshchawle@gmail.com>
2024-06-30 19:50:20 +0530

    File added

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [tracks, setTracks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [keyword, setKeyword] = useState("");
+
+  const getTracks = async () => {
+    setIsLoading(true);
+    let data = await fetch(
+      `https://v1.nocodeapi.com/tapeshchavle12/spotify/qCDTnysEkRLMmvrm/search?q=${
+        keyword === "" ? "trending" : keyword
+      }&type=track`
+    );
+    let convertedData = await data.json();
+    console.log(convertedData.tracks.items);
+    setTracks(convertedData.tracks.items);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getTracks();
+  }, []);
+
+  return (
+    <>
+      <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+
+          <div
+            className="collapse navbar-collapse d-flex justify-content-center"
+            id="navbarSupportedContent"
+          >
+            <input
+              value={keyword}
+              onChange={(e) => {
+                setKeyword(e.target.value);
+              }}
+              className="form-control me-2 w-75 "
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button onClick={getTracks} className="btn btn-outline-success">
+              Search
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <div className={`row ${isLoading ? "" : "d-none"}`}>
+          <div className="col-12 py-5 text-center">
+            <div
+              className="spinner-border"
+              style={{ width: "3rem", height: "3rem" }}
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {tracks.map((element) => {
+            return (
+              <div key={element.id} className="col-lg-3 col-md-6 py-2">
+                <div className="card">
+                  <img
+                    src={element.album.images[1].url}
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{element.name}</h5>
+                    <p className="card-text">
+                      Artist : {element.album.artists[0].name}
+                    </p>
+                    <p className="card-text">
+                      Release Date: {element.album.release_date}
+                    </p>
+                    <audio
+                      className="w-100"
+                      src={element.preview_url}
+                      controls
+                    ></audio>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
